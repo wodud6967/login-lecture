0628d57788326873b166d49cce0469d3f406c612
Iamjaeyoung <wodud6967@naver.com>
2022-11-25 18:48:20 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -14,4 +14,14 @@ function login(){
 
     };
     console.log(req);
+    console.log(JSON.stringify(req));
+   
+    fetch("/login", {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+        },
+        body : JSON.stringify(req)
+    });
 }
+
