bc1a4e1e6745d534343fe55b2182f577a31954a2
Iamjaeyoung <wodud6967@naver.com>
2022-11-27 19:56:59 +0900

    로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -22,6 +22,15 @@ function login(){
         method: "POST",
         headers: {"Content-Type": "application/json",},
         body: JSON.stringify(req),
-    }).then((res) => res.json()).then((res) => console.log(res));
+    }).then((res) => res.json())
+    .then((res) => {
+        if(res.success){
+            location.href ="/";
+        }else{
+            alert(res.msg);
+        }
+    }).catch((err) =>{
+        console.error(new Error("로그인중 에러발생"));
+    })
 }
 
